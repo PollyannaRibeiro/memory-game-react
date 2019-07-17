6018ab29afd7d05d3077edb1ba46577b51efa235
Polly <23667108+PollyannaRibeiro@users.noreply.github.com>
2019-07-17 12:29:50 +0100

    added classNames to each card

--- src/components/Card2/Card2.js
+++ src/components/Card2/Card2.js
@@ -9,16 +9,17 @@ class Card2 extends Component{
     cardOpened: PropTypes.bool.isRequired,
     cardMatch: PropTypes.bool.isRequired,
     cardIcon: PropTypes.string.isRequired,
+    cardError: PropTypes.bool.isRequired
   }
 
   render(){
     
     let className = this.props.cardOpened ? "open": ""
-
+    let cardMatch = this.props.cardMatch ? "match": ""
+    let cardError = this.props.cardError ? "error": ""
+    
     return(
-      <div className = {`card ${className} fa ${this.props.cardIcon}`}>
-
-      </div>
+      <div className = {`card ${className} fa ${this.props.cardIcon} ${cardMatch} ${cardError}`}></div>
     )
   }
 }
