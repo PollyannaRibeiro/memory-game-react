67bec6bc6a0fdd31511d70fe765ffef889344452
Polly <23667108+PollyannaRibeiro@users.noreply.github.com>
2019-08-04 14:07:17 +0100

    Updates

---
+++ precache-manifest.fe9090e553bef167509eee6a9290aa1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0d90d714a1973992dc09fe1474a992b",
+    "url": "/memory-game-react/index.html"
+  },
+  {
+    "revision": "1e40ebfea5b663587482",
+    "url": "/memory-game-react/static/css/main.058b3ad0.chunk.css"
+  },
+  {
+    "revision": "dbb4ac401103bee4492d",
+    "url": "/memory-game-react/static/js/2.4a38c754.chunk.js"
+  },
+  {
+    "revision": "1e40ebfea5b663587482",
+    "url": "/memory-game-react/static/js/main.8f430960.chunk.js"
+  },
+  {
+    "revision": "0735bb19372c7d478a5e",
+    "url": "/memory-game-react/static/js/runtime~main.7d97f3c5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/memory-game-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
